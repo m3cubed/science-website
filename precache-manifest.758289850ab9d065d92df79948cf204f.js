77b241e2d23da2c2efb67ac307bebe4abd8c1ba4
Mike Ma <mike.ma0426@gmail.com>
2018-11-03 18:32:56 -0400

    Updates

---
+++ precache-manifest.758289850ab9d065d92df79948cf204f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e48bbbd97937e4f9a7d687d4ae77d4b",
+    "url": "/science-website/static/media/teacher.5e48bbbd.png"
+  },
+  {
+    "revision": "5525ceb3faa7a99f6aa9ccb6c43e6a06",
+    "url": "/science-website/static/media/science.5525ceb3.jpg"
+  },
+  {
+    "revision": "77ec9f685f09166ae4dcafbf4edc6d31",
+    "url": "/science-website/static/media/school.77ec9f68.jpg"
+  },
+  {
+    "revision": "784b3f5469a0a5afa2d6b28eb270db5d",
+    "url": "/science-website/static/media/Earth.784b3f54.png"
+  },
+  {
+    "revision": "2ac0bf26eaa934e7745c",
+    "url": "/science-website/static/js/runtime~main.2ac0bf26.js"
+  },
+  {
+    "revision": "22e78e28f24396dffe5f",
+    "url": "/science-website/static/js/main.22e78e28.chunk.js"
+  },
+  {
+    "revision": "81e7877d5b86afc3f86f",
+    "url": "/science-website/static/js/1.81e7877d.chunk.js"
+  },
+  {
+    "revision": "5f91ca663c91a4fe1c86bdda0d44bff8",
+    "url": "/science-website/index.html"
+  }
+];
