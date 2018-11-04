a58ce2fce1e24ba24e90456c1ad5797fdb74b488
Mike Ma <mike.ma0426@gmail.com>
2018-11-03 23:26:06 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Cssbaseline from "@material-ui/core/CssBaseline";
 import Navbar from "./components/FrameWork/Navbar";
 
